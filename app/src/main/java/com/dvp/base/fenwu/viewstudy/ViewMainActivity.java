c3e31af9c316ae351e38e2b929fdcc6350cf9ac8
Administrator <dfwmaomao@163.com>
2016-06-24 14:58:32 +0800

    view原理学习首次提交

---
+++ app/src/main/java/com/dvp/base/fenwu/viewstudy/ViewMainActivity.java
@@ -0,0 +1,15 @@
+package com.dvp.base.fenwu.viewstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewMainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_main);
+    }
+}
